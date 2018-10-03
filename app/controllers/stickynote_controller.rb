eeb45548688fbb765b226ac13aed9188488c14b4
Kevin Hackenberg <khackenberg77@gmail.com>
2018-10-03 11:37:32 -0400

    fixed database connection, categories, added logo

--- app/controllers/stickynote_controller.rb
+++ app/controllers/stickynote_controller.rb
@@ -2,7 +2,11 @@ class StickynoteController < ApplicationController
     def index
         @sticky = Sticky.new
         @sticky.body = params["body"]
+        @sticky.category_id= params["catid"]
         @sticky.save
-        @stickynotes= Sticky.all
+        @todonotes= Sticky.where(category_id: "1")
+        @doingnotes= Sticky.where(category_id: "2")
+        @donenotes= Sticky.where(category_id: "3")
+        
     end
 end
